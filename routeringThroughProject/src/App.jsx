e9b64bdf8840e3f921f7a16eab127fa4b2af0923
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-08-11 22:14:25 +0530

    (half is done)

--- routeringThroughProject/src/App.jsx
+++ routeringThroughProject/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
-
+import Header from './component/Header/Header'
+import Footer from './component/Footer/Footer'
+import Home from './component/Home/Home'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    
     </>
   )
 }
--- routeringThroughProject/src/main.jsx
+++ routeringThroughProject/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './component/Home/Home.jsx'
+import About from './component/About/About'
+import Contact from './component/Contact/Contact'
+const router =createBrowserRouter([{
+  path:'/',
+  element:<Layout/>,
+  children: [
+    { path: "", element: <Home /> }, { path: "about", element: <About/> },{ path: "contact", element: <Contact /> }
+  ]
+}])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
