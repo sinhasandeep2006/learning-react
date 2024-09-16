0e710fa9e01ea469661462b5d0d106ad8e7c8e91
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-09-16 12:56:45 +0530

    todo

--- 10toDoListProject/src/App.jsx
+++ 10toDoListProject/src/App.jsx
@@ -4,9 +4,15 @@ import './App.css'
 import { Todoprovider } from './Contexts'
 
 function App() {
-  const [todos,setTodes]=useState([]);
+  const [todos, setTodes] = useState([]);
+  const addTodo = (todo) => {
+    setTodes((prev) => [{ id: Date.now(), ...todo }, ...prev])
+  }
+  const updateTodo = (id, todo) => {
+    setTodes((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo)))
+  }
   return (
-    <Todoprovider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
+    <Todoprovider value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
