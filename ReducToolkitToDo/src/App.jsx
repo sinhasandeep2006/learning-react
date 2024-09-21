ed1cddd613e8572d1229439341c918cae43d03d3
Sandeep Sinha <1233sandeepsinha@gmail.com>
2024-09-21 18:49:41 +0530

    completing this project

--- ReducToolkitToDo/src/App.jsx
+++ ReducToolkitToDo/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './Component/AddTodo'
+import Todos from './Component/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div>
-      oaaa la
+    <AddTodo/>
+    <Todos/>
     </div>
   )
 }
